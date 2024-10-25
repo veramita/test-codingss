f3404466032f8bd743f143ef2be7cc78fe3875cc
veramita <natsuaoranpu1@gmail.com>
2024-10-25 20:54:36 +0700

    tes 1

---
+++ tes.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    /*cout << "HELLO WORLD" << endl;
+
+    int a = 5;
+    int b = 7;
+    f(a>b){
+        cout << "a lebih besar dari b" << endl;
+    } else {
+        cout << "b lebih besar dari a" << endl;
+    }
+
+   int masuk;
+   cout << "masukkan angka";
+   cin >> masuk;
+   cout << "hasil " << masuk << endl; 
+
+   int i = 0;
+   for(i=0; i <= 5; i++){
+    cout << "angka ke-" << i << endl;
+   }*/
+   
+  int a[4] = {1,3,5,7};
+
+  for(int i = 0; i<a[i]; i++){
+    cout << "angka " << a[i] << endl;
+  }
+    return 0;
+}
